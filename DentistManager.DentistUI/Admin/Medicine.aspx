3c586af95d870c0061baeddb688b352a626aa9bf
batman86 <Marchosni86@gmail.com>
2014-02-28 03:51:12 +0200

    gg

---
+++ DentistManager.DentistUI/Admin/Medicine.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="Medicine.aspx.cs" Inherits="DentistManager.DentistUI.Admin.Medicine" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
--- DentistManager.Domain/Entities/DentistModel.Designer.cs
+++ DentistManager.Domain/Entities/DentistModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\MVC\projects\DentistSoft\DentistManager.Domain\Entities\DentistModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\work\Dentist\DentistManager\DentistSoft\DentistManager.Domain\Entities\DentistModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
